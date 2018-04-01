0f78d6b15c50682d4da648cc716ebb68f25d5879
ricardo <ricardo.aguirre.99@gmail.com>
2018-04-01 16:50:14 -0300

    subi avance de la guia de ejercicios

---
+++ operativos-pre-tp/guia_ejercicios_r/guia_ejercicio_1/Debug/src/so-commons-library/tests/unit-tests/build/subdir.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../src/so-commons-library/tests/unit-tests/build/test_bitarray.o \
+../src/so-commons-library/tests/unit-tests/build/test_config.o \
+../src/so-commons-library/tests/unit-tests/build/test_dictionary.o \
+../src/so-commons-library/tests/unit-tests/build/test_list.o \
+../src/so-commons-library/tests/unit-tests/build/test_queue.o \
+../src/so-commons-library/tests/unit-tests/build/test_string.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
---
+++ operativos-pre-tp/guia_ejercicios_r/guia_ejercicio_2/Debug/src/so-commons-library/src/build/commons/subdir.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../src/so-commons-library/src/build/commons/bitarray.o \
+../src/so-commons-library/src/build/commons/config.o \
+../src/so-commons-library/src/build/commons/error.o \
+../src/so-commons-library/src/build/commons/log.o \
+../src/so-commons-library/src/build/commons/process.o \
+../src/so-commons-library/src/build/commons/string.o \
+../src/so-commons-library/src/build/commons/temporal.o \
+../src/so-commons-library/src/build/commons/txt.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
