cf4a1205f203815a7a354e79d968fa82a49df93f
Fernando Arellano <f.arellano919@gmail.com>
2018-06-07 14:39:55 -0300

    resuelvo conflictos

--- compile.sh
+++ compile.sh
@@ -3,41 +3,25 @@ echo ""
 echo ...::: Iniciando compilación de los procesos :::...
 echo ""
 
-<<<<<<< HEAD
-cd esi/
-=======
 cd esi/src
->>>>>>> origin/versionVieja
 make clean
 make
 
 cd ..
 
-<<<<<<< HEAD
-cd coordinador/
-=======
 cd coordinador/src
->>>>>>> origin/versionVieja
 make clean
 make
 
 cd ..
 
-<<<<<<< HEAD
-cd planificador/
-=======
 cd planificador/src
->>>>>>> origin/versionVieja
 make clean
 make
 
 cd ..
 
-<<<<<<< HEAD
-cd instancia
-=======
 cd instancia/src
->>>>>>> origin/versionVieja
 make clean
 make
 
